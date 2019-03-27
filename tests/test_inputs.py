44d30431c52631544425f08b3c95be0efa313e36
Pablo Abril <pabril@student.42.fr>
2019-03-27 15:49:16 +0100

    added some tests that Travis can run

--- tests/test_inputs.py
+++
@@ -1,5 +0,0 @@
-def func_add(a, b):
-    return a + b
-
-def test_func():
-    assert func_add(1,2) == 3
---
+++ travis_test/test_parser.py
@@ -0,0 +1,38 @@
+import pytest
+import os
+from exception import ParsingError
+from parse import parse_file
+
+
+def test_incorrect_files():
+    list_files = [
+        "",
+        ".",
+        "..",
+        "/dev/random",
+        "/dev/null"
+    ]
+    print("hello")
+    run_tests(list_files)
+
+def test_wrong_files():
+    run_tests(get_files("./test_files", pattern="f_"))
+    run_tests(get_files("./pablo_test/incorrect"))
+
+def run_tests(file_list):
+    for file_path in file_list:
+        tab = []
+        with pytest.raises(ParsingError) as e:
+            print(f"Testing file : {file_path}")
+            parse_file(file_path, tab)
+
+
+def get_files(path, pattern=""):
+	files = []
+	for file in os.listdir(path):
+		file_path = path + "/" + file
+		if os.path.isdir(file_path):
+			files.extend(get_files(file_path))
+		elif os.path.isfile(file_path) and pattern in file:
+			files.append(file_path)
+	return (files)
