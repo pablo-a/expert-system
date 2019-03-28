03ec2e07a660e8086cddea76e4f029e23f3e1b5d
Pablo Abril <pabril@student.42.fr>
2019-03-28 15:02:49 +0100

    add a first draft to solve a Fact. works for now, need more testing.

--- engine.py
+++ engine.py
@@ -24,6 +24,52 @@ class Engine:
         Input: ?XZY
         """
         pass
+########################## THE METHOD ####################################
+    def solve(self):
+        solution = []
+        for query in self.query:
+            solution.append(self.solve_fact(query))
+            self.already_checked = []
+        return solution
+
+    def solve_fact(self, fact):
+        if fact.name in self.already_checked:
+            print("\tAlready been here, stopping.")
+            return False
+        self.already_checked.append(fact.name)
+
+        if self.fact_is_true(fact):
+            return True
+
+        # Fetch a condition of truth
+        try:
+            rule = self.get_rule(fact)
+        except KeyError: # means no precendence rule.
+            print(f"\tno rule for fact {fact}")
+            return False
+
+        print(f"\trule found for fact {fact} : {rule}")
+        # If dependency is just a Fact (A => B), recursive call
+        if isinstance(rule, Fact):
+            return self.solve_fact(rule)
+        # else it is operators to resolve.
+        return rule.resolve_to_true()
+
+###########################################################################
+
+    def fact_is_true(self, fact):
+        # Check if Fact already in knowledge base
+        for elem in self.facts:
+            if fact.name == elem.name:
+                print(f"\tfact {fact.name} is in knowledge base")
+                return True
+        print(f"\tfact {fact.name} is not in knowledge base")
+        return False
+
+########################################################################
+
+    def get_rule(self, fact):
+        return self.rules[fact.name]
 
 
 class Fact:
