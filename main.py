9d0544d7ec1f00986407b235fb673dfead931e98
Pablo Abril <pabril@student.42.fr>
2019-03-30 13:05:22 +0100

    setup main story of parsing in main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
+# FIXME: rename parse module to something like lexer, syntax_checker, ...
 import parse
+import parser
+from engine import Engine
 from rules_json import rules_json
 from exception import ParsingError
 
@@ -16,8 +19,18 @@ def main(file_path):
     except ParsingError as e:
         exit(e)
 
+    # break into different type of statements
+    rules, facts, query = parse.break_input_statement_type(tab)
+
+    # setup engine
+    engine = Engine()
+    parser.setup_engine_rules(engine, rules)
+    parser.setup_engine_facts(engine, facts)
+    parser.setup_engine_query(engine, query)
+
     # Solving
-    rules_json(tab)
+    # rules_json(tab)
+    # engine.solve()
 
 
 def raise_parsing_error(msg):
