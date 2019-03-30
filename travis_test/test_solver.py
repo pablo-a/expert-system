4a0965cfd35514090478e056f25af6ca33743324
Pablo Abril <pabril@student.42.fr>
2019-03-30 10:40:24 +0100

    add a logger to test + add 2 tests for same conclusion

--- travis_test/test_solver.py
+++ travis_test/test_solver.py
@@ -4,6 +4,18 @@ sys.path.extend(["../"])
 
 from engine import Engine, Fact
 from operations import And, Or
+from termcolor import colored
+
+
+def log_engine(run_test):
+    def wrap():
+        print(colored(f"\n\nTEST : {run_test.__name__}\n", "red"))
+        print(colored("\nSolution steps :\n\t", 'green'))
+        run_test()
+        print(colored("\nFacts :\n", 'green'))
+        print(colored(f"\n{my_engine}", 'magenta'))
+        print(colored(f"\nTest passed ? ", "yellow"), end="")
+    return wrap
 
 # TESTING
 my_engine = Engine()
@@ -45,19 +57,47 @@ my_engine.rules[query2.name] = second_rule
 
 rule_A.flatten()
 
-
+@log_engine
 def test_simple():
     # Already in Facts
     my_engine.add_query("?A")
     assert my_engine.solve() == [True]
     
+@log_engine
 def test_backward():
     # 2 steps backward
     my_engine.add_query("?X", empty=True)
     assert my_engine.solve() == [True]
 
+@log_engine
 def test_infinite_loop():
-    # Infinite loop
+    "Infinite Loop if try to resolve"
+    # reset facts.
     my_engine.add_facts("=", empty=True)
     my_engine.add_query("?Y", empty=True)
     assert my_engine.solve() == [False]
+
+@log_engine
+def test_same_conclusion():
+    "several rules for 1 conclusion"
+    my_engine.add_facts("=Q")
+    my_engine.add_query("?X", empty=True)
+    #add new rule : Q => X
+    f = Fact("X", my_engine)
+    rule = Fact("Q", my_engine)
+    my_engine.add_rule(f, rule)
+    #solve
+    assert my_engine.solve() == [True]
+
+@log_engine
+def test_same_conclusion_2():
+    "several nested rules for 1 conclusion"
+    my_engine.add_facts("=R", empty=True)
+    # Add new rule : : R | S => X
+    target = Fact("X", my_engine)
+    new_condition = Or(Fact("R", my_engine), Fact("S", my_engine))
+    my_engine.add_rule(target, new_condition)
+
+    #solve
+    assert my_engine.solve() == [True]
+
