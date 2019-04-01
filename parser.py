3cb0ab9b3b26dfc318e9952c5d517b5968f0faec
Pablo Abril <pabril@student.42.fr>
2019-04-01 15:17:13 +0200

    implement missing functions in parser.

--- parser.py
+++ parser.py
@@ -1,4 +1,8 @@
 from fact import Fact
+import operations
+import util
+import logging
+import re
 
 def setup_engine_query(engine, input):
     for line in input:
@@ -9,9 +13,16 @@ def setup_engine_facts(engine, input):
         engine.add_facts(line)
 
 def setup_engine_rules(engine, input):
+    logging.debug(f"SETUP ENGINE RULES\n")
+    logging.debug(f"non parsed rules : {util.print_list(input)}")
+
     input_without_bi = handle_biconditional(engine, input)
-    tokenized_input = cut_statement(input_without_bi)
-    handle_operations_in_conclusion(engine, tokenized_input)
+    logging.debug(f"handle <=> : {util.print_list(input_without_bi)}")
+
+    split_neg_operator = check_negative_operator(input_without_bi)
+    logging.debug(f"Neg operator : {util.print_list(split_neg_operator)}")
+
+    tokenized_input = cut_statement(split_neg_operator)
     parse_all_rules(engine, tokenized_input)
 
 ########################### BICONDITIONAL #####################################
@@ -24,7 +35,7 @@ def handle_biconditional(engine, input):
             input.append(opposit_statement)
 
     # replace "<=>" by "=>" in all statements.
-    input_without_bi = map(lambda x: x.replace("<=>", "=>"), input)
+    input_without_bi = list(map(lambda x: x.replace("<=>", "=>"), input))
     return input_without_bi
 
 def is_biconditional(statement):
@@ -51,22 +62,28 @@ def cut_statement(input):
         tokenized_input.append(new_statement)
     return tokenized_input
 
-######################## OPERATIONS IN CONCLUSION #############################
+####################### CHECK NEGATIVE OPERATOR ###############################
 
-# TODO: Implement
-def handle_operations_in_conclusion(engine, input):
-    """
-        "A + B" => ["A", "B"]
-    """
-    # TODO: Handle OR/XOR operations ?
-    pass
+def check_negative_operator(rules):
+    new_rules = []
+    for statement in rules:
+        splitted = statement.split()
+        for index, elem in enumerate(splitted):
+            if re.match("\![a-zA-Z]$", elem):
+                splitted[index:index+1] = ["!", elem[1]]
+        joined = " ".join(splitted)
+        new_rules.append(joined)
+    return new_rules
+                
 
 ###################### PARSING RULES ##########################################
 
 def parse_all_rules(engine, input):
     for statement in input:
+        logging.debug(f"Parsing Statement : {statement}")
         conclusion_array = parse_conclusion(engine, statement['conclusion'])
         rule = parse_rule(engine, statement['rule'])
+        logging.debug(f"\n\tEND PARSING : {rule} => {conclusion_array}")
 
         for conclusion in conclusion_array:
             engine.add_rule(conclusion, rule)
@@ -75,31 +92,50 @@ def parse_all_rules(engine, input):
 
 # TODO: Implement
 def parse_conclusion(engine, conclusions):
-    "return an array of Fact from an array of conclusions"
+    "return an array of Facts from conclusion"
+    tokens = conclusions.split()
+    convert_to_fact_instances(engine, tokens)
+    if len(tokens) == 1:
+        return tokens
+    for token in tokens:
+        pass
     return []
 
 
 ############ PRIORITY #####################
 
-operation_priority = ["!", "+", "|", "^"]
+operation_priority = ["+", "|", "^"]
+operation_classes  = [operations.And, operations.Or, operations.Xor]
 
-# TODO: Implement
 def parse_rule(engine, rule):
     """
         Return a Fact or Operation insctance equivalent to the rule.
         Input : A + (B | C)
         Output : And(A, Or(B, C))
     """
+    logging.debug(f"Parsing Rule : \"{rule}\"")
+
     tokens = rule.split()
+    # literal to Object : "A" => Fact("A")
     convert_to_fact_instances(engine, tokens)
+    logging.debug(f"Fact converted: {util.print_list(tokens)}")
+
+    # remove parenthesis by simplifying content.
     tokens = parse_parenthesis(tokens)
-    print(tokens)
+    logging.debug(f"rm parenthesis : {util.print_list(tokens)}")
+
+    # simplify rule while handling priority.
+    rule = parse_operations_priority(tokens)
+    if len(rule) == 1:
+        logging.debug(f"parsed operations : {util.print_list(rule)}")
+        return rule[0]
+    logging.critical(f"rule after operation parsing : {util.print_list(rule)}")
     return []
 
 def convert_to_fact_instances(engine, tokens):
     "Convert facts literals to instances of Fact"
     for index,elem in enumerate(tokens):
-        if isinstance(elem, str):
+        if isinstance(elem, str) and elem.isalpha():
             tokens[index] = Fact(elem, engine)
 
 def parse_parenthesis(rule):
@@ -114,14 +150,63 @@ def parse_parenthesis(rule):
             break
     opening_bracket = positions.pop()
     rule[opening_bracket:closing_bracket+1] = parse_operations_priority(rule[opening_bracket, closing_bracket+1])
-    parse_parenthesis(rule)
+    # Tail recursion : simplify the simplified rule until no parenthesis.
+    return parse_parenthesis(rule)
 
 def parse_operations_priority(tokens):
-    for operator in operation_priority:
-        tokens = parse_operator(operator, tokens)
+    tokens = parse_negative(tokens)
+    logging.debug(f"remove negation literals : {util.print_list(tokens)}")
+    for operator, op_class in zip(operation_priority, operation_classes):
+        tokens = parse_operator(operator, op_class, tokens)
     return tokens
     
-def parse_operator(operator, rule):
-    return []
+    if len(rule) == 1:
+        return rule
+
+def parse_operator(operator, op_class, rule):
+    should_pass_next = 0
+    if len(rule) <= 2:
+        return rule
+    new_rule = []
+    for left_token, middle_token, right_token in util.get_three_by_three(rule):
+        if should_pass_next > 0:
+            should_pass_next -= 1
+            continue
+        if middle_token == operator:
+            obj = op_class(left_token, right_token)
+            new_rule.append(obj)
+            should_pass_next = 2
+        else:
+            new_rule.append(left_token)
+
+    # add remaining
+    if should_pass_next == 0:
+        new_rule.append(middle_token)
+        new_rule.append(right_token)
+    if should_pass_next == 1:
+        new_rule.append(right_token)
+
+    return new_rule
+
+def parse_negative(rule):
+    new_rule = []
+    should_pass_next = False
+
+    if len(rule) < 2:
+        return rule
+
+    for token, next_token in util.get_two_by_two(rule):
+        if should_pass_next:
+            should_pass_next = False
+            continue
+        if token == "!" and (isinstance(next_token, Fact) or issubclass(next_token, operations.Operator)):
+            obj = operations.Not(next_token)
+            new_rule.append(obj)
+            should_pass_next = True
+        else:
+            new_rule.append(token)
+    if not should_pass_next:
+        new_rule.append(next_token)
+    return new_rule
 
 
